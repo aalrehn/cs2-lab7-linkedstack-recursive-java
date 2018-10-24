84133d4ff5ce696550aff557642ce4017b451bda
Ameen <aalrehn@luc.edu>
2018-10-24 04:19:27 +0000

    All tests pass except for one

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -6,15 +6,23 @@ public class ReverseLines {
 
   public static void main(final String[] args) {
     // TODO read successive input lines until EOF, then print out in reverse order
-
-
+    LinkedStack<String> stack = new LinkedStack<String>();
+  
 
     final Scanner input = new Scanner(System.in);
     while (input.hasNextLine()) {
       final String line = input.nextLine();
+      stack.push(line);
+      if(line == " "){
+          break;
+      }
+      
 
 
 
+    }
+    while (!stack.isEmpty()){
+        System.out.println(stack.pop());
     }
 
 
